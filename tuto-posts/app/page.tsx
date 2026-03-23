2d49c4474d2d428c676f861459d098c29f88b8da
Guillaume <guillaume.robidet@outlook.fr>
2026-03-23 16:40:29 +0100

    first commit

--- tuto-posts/app/page.tsx
+++ tuto-posts/app/page.tsx
@@ -1,16 +1,32 @@
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
+
+
 export default async function Home() {
-  /*Récupère tous les posts (100) de l'API placeholder : */ 
-  // const resultat = await fetch("https://jsonplaceholder.typicode.com/posts/"); 
+  /*Récupère tous les posts (100) de l'API placeholder : */
+  const resultat = await fetch("https://jsonplaceholder.typicode.com/posts/");
 
-  /*Récupère 1 seul post en particulier: */ 
-  const resultat = await fetch("https://jsonplaceholder.typicode.com/posts/1"); 
+  /*Récupère 1 seul post en particulier: */
+  // const resultat = await fetch("https://jsonplaceholder.typicode.com/posts/1"); 
 
-  const post = await resultat.json(); /*Requète stockée temporairement*/
-  console.log(post); /*Résultat dans la console*/
+  const posts = await resultat.json(); /*Requète stockée temporairement*/
+  console.log(posts); /*Résultat dans la console*/
 
   return (
     <div>
       <h1>Post</h1>
+      {posts.map((post: Post) => (
+        <>
+          <p>{post.id}</p>
+          <p>{post.body}</p>
+        </>
+      ))}
+
     </div>
   );
 }
